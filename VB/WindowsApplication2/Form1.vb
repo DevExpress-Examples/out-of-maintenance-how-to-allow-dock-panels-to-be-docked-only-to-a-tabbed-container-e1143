6e3851d24d58b0d69e87384d1aa8849f5d2c5477
DevExpressExampleBot <fakeEmail>
2018-11-01 17:57:08 +0300

    Source auto update [skip ci]

---
+++ VB/WindowsApplication2/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports DevExpress.XtraBars.Docking
+
+Namespace WindowsApplication2
+    Partial Public Class Form1
+        Inherits Form
+
+        Private helper As DockHelper
+
+        Public Sub New()
+            InitializeComponent()
+            helper = New DockHelper(dockManager1)
+        End Sub
+
+        Private Sub checkEdit1_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles checkEdit1.CheckedChanged
+            Dim check As DevExpress.XtraEditors.CheckEdit = TryCast(sender, DevExpress.XtraEditors.CheckEdit)
+            If check IsNot Nothing Then
+                helper.AllowDockToTabbedContainer = check.Checked
+            End If
+        End Sub
+    End Class
+End Namespace
